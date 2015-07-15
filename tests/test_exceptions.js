543cd8feb038c40faed16659c0027943033d9e25
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-07-15 11:47:36 +0200

    providing a generic Exception

--- tests/test_exceptions.js
+++ tests/test_exceptions.js
@@ -1,6 +1,11 @@
 var exceptions = require('../lib/exceptions');
 
 describe('Exceptions list', function() {
+  it('should return an Error for a generic Exception object', function() {
+    var fn = function() { throw exceptions.Exception() };
+    expect(fn).to.throw(Error);
+    expect(fn).to.throw('Exception');
+  });
   it('should return an Error for AttributeError exception', function() {
     var fn = function() { throw exceptions.AttributeError() };
     expect(fn).to.throw(Error);
