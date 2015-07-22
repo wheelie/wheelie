f8d46f2cf4454dd85baa26f46aec86b8e9f9a24f
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-07-22 11:10:29 +0200

    the internal Logger is now initialized during the require statement

--- tests/test_logger.js
+++ tests/test_logger.js
@@ -1,12 +1,6 @@
-var LoggerClass = require('../lib/logging/logger');
+var logger = require('../lib/logging/logger');
 
 describe('A Logger', function () {
-  var logger;
-
-  beforeEach(function() {
-    logger = new LoggerClass();
-  });
-
   it('should have DEBUG as default write level', function() {
     expect(logger.getWriteLevel()).to.be.equal('DEBUG');
   });
