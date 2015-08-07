e3eb284de950c9ceef740792985d63d7c7b73df8
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-08-07 17:30:10 +0200

    better descriptions for Wheelie build tests

--- tests/test_gulp.js
+++ tests/test_gulp.js
@@ -3,7 +3,7 @@ var Wheelie = require('../lib/cli/wheelie');
 var Task = require('../lib/models/task');
 
 
-describe('Wheelie, during the build process,', function() {
+describe('Wheelie,', function() {
   var wheelie;
   var sandbox = sinon.sandbox;
 
@@ -45,111 +45,140 @@ describe('Wheelie, during the build process,', function() {
     expect(gulpTask.calledWith('task', [], undefined)).to.be.true;
   });
 
-  it('should call the config() callback for each task', function() {
-    var emptyFn = function() {};
-    var config = sandbox.stub();
+  describe('tasks configuration loader', function() {
+    it('should call the config() callback for each task', function() {
+      var emptyFn = function() {};
+      var config = sandbox.stub();
 
-    var task = new Task('task', [], emptyFn, config);
+      var task = new Task('task', [], emptyFn, config);
 
-    wheelie.add(task);
-    wheelie.build();
+      wheelie.add(task);
+      wheelie.build();
 
-    expect(config.calledOnce).to.be.true;
+      expect(config.calledOnce).to.be.true;
+    });
+
+    it('should pass the global settings to each task config() callback', function() {
+      var emptyFn = function() {};
+      var config = sandbox.stub();
+
+      var task = new Task('task', [], emptyFn, config);
+
+      wheelie.add(task);
+      wheelie.build();
+
+      expect(config.calledWith(wheelie.options)).to.be.true;
+    });
+
+    it('should provide a "dest" folder according to production status', function() {
+      var spy = sandbox.spy(wheelie, "getDest");
+
+      var emptyFn = function() {};
+      var config = sandbox.stub();
+
+      var task = new Task('task', [], emptyFn, config);
+
+      wheelie.add(task);
+      wheelie.build();
+
+      var passedSettings = config.getCall(0).args[0];
+
+      expect(spy.calledOnce).to.be.true;
+      expect(passedSettings.dest).to.be.not.undefined;
+    });
+
+    it('should prevent global settings pollution between plugins execution', function() {
+      var gulpTask = sandbox.stub();
+      var run = function() { return gulpTask; };
+
+      var maliciousConfig = function(globals) {
+        globals.dest = '/';
+        return {};
+      }
+
+      var goodConfig = sandbox.stub();
+
+      var task = new Task('task', [], run, maliciousConfig);
+      var anotherTask = new Task('task_2', ['task'], run, goodConfig);
+
+      wheelie.add([task, anotherTask]);
+      wheelie.build();
+
+      var passedSettings = goodConfig.getCall(0).args[0];
+      expect(passedSettings.dest).to.be.not.equal('/');
+    });
   });
 
-  it('should pass the global settings to each task config() callback', function() {
-    var emptyFn = function() {};
-    var config = sandbox.stub();
+  describe('tasks executor', function() {
+    it('should apply a patch to the generated config() object', function() {
+      var run = sandbox.stub();
+      var config = function() { return {'key': 'value'}; };
+      var patch = {'patch': 'applied'};
 
-    var task = new Task('task', [], emptyFn, config);
+      var task = new Task('task', [], run, config);
+      wheelie.add(task);
+      wheelie.update('task', patch);
+      wheelie.build();
 
-    wheelie.add(task);
-    wheelie.build();
+      args = run.getCall(0).args;
+      expect(args[0]).to.be.eql(wheelie.gulp);
+      expect(args[1]).to.be.eql({'key': 'value', 'patch': 'applied'});
+    });
 
-    expect(config.calledWith(wheelie.options)).to.be.true;
-  });
+    it('should apply all patches to the generated config() object', function() {
+      var run = sandbox.stub();
+      var config = function() { return {'key': 'value'}; };
+      var firstPatch = {'patch': 'applied'};
+      var secondPatch = {'key': 'changed'};
 
-  it('should provide the gulp instance and the config() generated object to the run() callback', function() {
-    var run = sandbox.stub();
-    var config = function() { return object; };
-    var object = {'key': 'value'};
+      var task = new Task('task', [], run, config);
+      wheelie.add(task);
+      wheelie.update('task', firstPatch);
+      wheelie.update('task', secondPatch);
+      wheelie.build();
 
-    var task = new Task('task', [], run, config);
-    wheelie.add(task);
-    wheelie.build();
+      args = run.getCall(0).args;
+      expect(args[0]).to.be.eql(wheelie.gulp);
+      expect(args[1]).to.be.eql({'key': 'changed', 'patch': 'applied'});
+    });
 
-    args = run.getCall(0).args;
-    expect(args[0]).to.be.eql(wheelie.gulp);
-    expect(args[1]).to.be.eql(object);
-  });
+    it('should pass the gulp instance to the run() callback', function() {
+      var run = sandbox.stub();
+      var config = function() { return object; };
+      var object = {'key': 'value'};
 
-  it('should apply a patch to the generated config() object', function() {
-    var run = sandbox.stub();
-    var config = function() { return {'key': 'value'}; };
-    var patch = {'patch': 'applied'};
+      var task = new Task('task', [], run, config);
+      wheelie.add(task);
+      wheelie.build();
 
-    var task = new Task('task', [], run, config);
-    wheelie.add(task);
-    wheelie.update('task', patch);
-    wheelie.build();
+      args = run.getCall(0).args;
+      expect(args[0]).to.be.eql(wheelie.gulp);
+    });
 
-    args = run.getCall(0).args;
-    expect(args[0]).to.be.eql(wheelie.gulp);
-    expect(args[1]).to.be.eql({'key': 'value', 'patch': 'applied'});
-  });
+    it('should pass the config() object to the run() callback', function() {
+      var run = sandbox.stub();
+      var config = function() { return object; };
+      var object = {'key': 'value'};
 
-  it('should apply all patches to the generated config() object', function() {
-    var run = sandbox.stub();
-    var config = function() { return {'key': 'value'}; };
-    var firstPatch = {'patch': 'applied'};
-    var secondPatch = {'key': 'changed'};
+      var task = new Task('task', [], run, config);
+      wheelie.add(task);
+      wheelie.build();
 
-    var task = new Task('task', [], run, config);
-    wheelie.add(task);
-    wheelie.update('task', firstPatch);
-    wheelie.update('task', secondPatch);
-    wheelie.build();
+      args = run.getCall(0).args;
+      expect(args[1]).to.be.eql(object);
+    });
 
-    args = run.getCall(0).args;
-    expect(args[0]).to.be.eql(wheelie.gulp);
-    expect(args[1]).to.be.eql({'key': 'changed', 'patch': 'applied'});
-  });
+    it('should pass the Wheelie global options to the run() callback', function() {
+      var run = sandbox.stub();
+      var config = function() { return object; };
+      var object = {'key': 'value'};
 
-  it('should prevent global settings pollution between plugins execution', function() {
-    var gulpTask = sandbox.stub();
-    var run = function() { return gulpTask; };
+      var task = new Task('task', [], run, config);
+      wheelie.add(task);
+      wheelie.build();
 
-    var maliciousConfig = function(globals) {
-      globals.dest = '/';
-      return {};
-    }
-
-    var goodConfig = sandbox.stub();
-
-    var task = new Task('task', [], run, maliciousConfig);
-    var anotherTask = new Task('task_2', ['task'], run, goodConfig);
-
-    wheelie.add([task, anotherTask]);
-    wheelie.build();
-
-    var passedSettings = goodConfig.getCall(0).args[0];
-    expect(passedSettings.dest).to.be.not.equal('/');
-  });
-
-  it('should provide a "dest" folder according to production status', function() {
-    var spy = sandbox.spy(wheelie, "getDest");
-
-    var emptyFn = function() {};
-    var config = sandbox.stub();
-
-    var task = new Task('task', [], emptyFn, config);
-
-    wheelie.add(task);
-    wheelie.build();
-
-    var passedSettings = config.getCall(0).args[0];
-
-    expect(spy.calledOnce).to.be.true;
-    expect(passedSettings.dest).to.be.not.undefined;
+      args = run.getCall(0).args;
+      expect(args[2]).to.be.eql(wheelie.options);
+    });
   });
 });
