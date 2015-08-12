6342a0005a960c52ab540020fe5057d85974d50b
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-08-12 12:03:14 +0200

    deep extend takes care of arrays object

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -98,6 +98,14 @@ describe('Utilities:', function() {
       expect(newObj).to.be.deep.eql({'settings': {'foo': 'bar'}});
     });
 
+    it('should deep merge two JavaScript objects in a new one, updating even arrays', function() {
+      var oldObj = {'settings': {'foo': []}};
+      var updateObj = {'settings': {'foo': ['with_bar']}};
+
+      var newObj = utils.extend({}, oldObj, updateObj);
+      expect(newObj).to.be.deep.eql({'settings': {'foo': ['with_bar']}});
+    });
+
     it('should deep merge two JavaScript objects replacing the old one, updating their values', function() {
       var oldObj = {'settings': {'foo': 'foo'}};
       var updateObj = {'settings': {'foo': 'bar'}};
